73b98a0020df126c94aac2a7bc3f1ad602240bb3
angiefandul <152406516+angiefandul@users.noreply.github.com>
2024-08-18 17:04:57 -0600

    lesson 5

---
+++ tests/lesson5.js
@@ -0,0 +1,20 @@
+//Logical "AND"
+
+//console.log(true && false) //all values have to be true for expression to be true
+
+//Logical "OR"
+//console.log(true || false) // any values should be TRUE for the expression to be true
+
+
+var ageIsMoreThanEighteen = false
+var isUSCitizen = true
+
+var eligibilityForDriverLicense = ageIsMoreThanEighteen && isUSCitizen
+console.log('This customer is eligible for DL: ' + eligibilityForDriverLicense);
+
+// Logical "NOT"
+//console.log(!true)
+console.log(6 !== 10)
+
+
+
